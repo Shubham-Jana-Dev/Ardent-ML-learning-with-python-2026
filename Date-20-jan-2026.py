e991c4790cc3fa4533f4b1d56cec073012d12b14
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-20 16:30:49 +0530

    feat: Find the leargest number form a list without using max function

---
+++ Date-20-jan-2026.py
@@ -0,0 +1,15 @@
+# Find out the leargest element form a list whithout using max() function.
+"""
+h = [23,11,43,22,34,54,88,12,36,2]
+t = h[0]
+for i in h:
+    if i>t:
+        t=i
+print(t)
+"""
+# Using max() functions...
+"""
+y = max(h)
+print(y)
+"""
+    
