6a9c82e4568fc549bbc52de91ac15df5a9d6bc39
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-21 04:56:47 +0530

    feat: create a list containing the frist 10 natural numbers using a loop.

---
+++ Assignment_01.py
@@ -0,0 +1,8 @@
+# Python Practice Questions: Lists and Sets
+# Lists
+# 1.  List Creation: Create a list containing the first 10 natural numbers using a loop.
+My_list = [1]
+h = My_list[0]
+for i in range(2,11):
+        My_list.insert(i,i)
+print(My_list)
