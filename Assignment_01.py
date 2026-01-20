52d3c98b206396f4379d5f9cafb6d1e649d0aedb
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-21 05:19:14 +0530

    feat: Positive Filter: Given a list of integers, create a new list containing only positive numbers.

--- Assignment_01.py
+++ Assignment_01.py
@@ -2,7 +2,14 @@
 # Lists
 # 1.  List Creation: Create a list containing the first 10 natural numbers using a loop.
 My_list = [1]
-h = My_list[0]
+
 for i in range(2,11):
         My_list.insert(i,i)
+print(My_list)
+# 3 Positive Filter: Given a list of integers, create a new list containing only positive numbers.
+My_list = [11,-2,33,4,54,23,-6,43,-9,87,6,-7]
+print(My_list)
+for j in My_list:
+        if j<0:
+            My_list.remove(j)
 print(My_list)
