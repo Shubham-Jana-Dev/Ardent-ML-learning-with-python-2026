3095afef12febdea9beb355cacbfa95c00e3751a
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-20 16:57:37 +0530

    feat: add examples of list methods len(), insert(), append()

--- Date-20-jan-2026.py
+++ Date-20-jan-2026.py
@@ -12,4 +12,13 @@ print(t)
 y = max(h)
 print(y)
 """
+My_list = [12,23,12,44,32,54,76,44,32,64]
+print("The length of this list is: ", len(My_list)) # len() function helps to find out the length of a list
+My_list.insert(5,64) # .insert(index,value) use to add new value or item in a particular position (index) of a list
+print("Updated list after inserting 64 : ",My_list)
+My_list.append(78) # .append() function can only add 1 single item (number or string) in the very last of the list
+print(My_list)
+foods = ["Momo","Ice-cream"]
+foods.append("Fish fry") # .append() can also be used to add string to a list (a list of strings)...
+print(foods)
     
