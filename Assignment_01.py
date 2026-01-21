54264413dc2f9c4aa614f3d9c9ac0d1f4e0c78e6
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-01-21 05:50:54 +0530

    feat: count occurrences: count how many times a specific element appears in a list

--- Assignment_01.py
+++ Assignment_01.py
@@ -6,14 +6,14 @@ My_list = [1]
 for i in range(2,11):
         My_list.insert(i,i)
 print(My_list)
-# 3 Positive Filter: Given a list of integers, create a new list containing only positive numbers.
+# 2. Positive Filter: Given a list of integers, create a new list containing only positive numbers.
 My_list = [11,-2,33,4,54,23,-6,43,-9,87,6,-7]
 print(My_list)
 for j in My_list:
         if j<0:
             My_list.remove(j)
 print(My_list)
-# 4 Element Swap: Swap the first and last elements of a list.
+# 3. Element Swap: Swap the first and last elements of a list.
 The_list = [23,11,43,22,34,54,88,12,36,2]
 print(The_list)
 first_element = The_list[0]
@@ -23,3 +23,10 @@ The_list.append(first_element)
 The_list.remove(last_element)
 The_list.insert(0,last_element)
 print(The_list)
+# 4. Count Occurrences: Count how many times a specific element appears in a list.
+future_blood = ["Gov. school","LMG or LMB","Private school","Summit school","LMG or LMB","CBSE school","LMG or LMB","BCM International","LMG or LMB","St. Xavier","LMG or LMB","Gov. school"]
+count = 0
+for i in future_blood:
+       if i == "LMG or LMB":
+            count += 1
+print(count)
