21b169981281d28e8b2146e144f5906d35f8b325
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-25 05:52:23 +0530

    feat: found the index of the elements those are less than 10.

---
+++ Feb-23-2026/class_work.py
@@ -0,0 +1,17 @@
+# find out 5 from the list [12,334,5,56,7,453,6,67]
+import numpy as np
+def finding_element():
+    arr = np.array([12,334,5,56,7,453,6,67])
+    terget = np.where(arr == 5)
+    print(terget)
+# Print the shape of a list
+
+def print_shape():
+    arr1 = np.array([12,23,34,45,55,67,14,46])
+    print(arr1.shape)
+# print_shape()
+def print_found_group_of_elements():
+    arr2 = np.array([11,23,12,22,45,65,78,98,76,56,35,49,59,27,44,1,4,7,9,3,78,6])
+    smaller = np.where(arr2<10)
+    print(smaller)
+print_found_group_of_elements()
