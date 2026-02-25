c94a5af96b81ba8cd18d4b65d0b71d844ab9bc86
Shubham-Jana-Dev <devshubhamjana@gmail.com>
2026-02-25 06:13:59 +0530

    feat: sort elements and sort in reverse order

--- Feb-23-2026/class_work.py
+++ Feb-23-2026/class_work.py
@@ -10,8 +10,22 @@ def print_shape():
     arr1 = np.array([12,23,34,45,55,67,14,46])
     print(arr1.shape)
 # print_shape()
+
 def print_found_group_of_elements():
     arr2 = np.array([11,23,12,22,45,65,78,98,76,56,35,49,59,27,44,1,4,7,9,3,78,6])
     smaller = np.where(arr2<10)
     print(smaller)
-print_found_group_of_elements()
+# print_found_group_of_elements()
+
+# Sort the list by numpy.
+def sort_by_numpy():
+    arr2 = np.array([11,23,12,22,45,65,78,98,76,56,35,49,59,27,44,1,4,7,9,3,78,6])
+    sorted_list = np.sort(arr2)
+    print(sorted_list)
+sort_by_numpy()
+
+def sort_by_numpy_in_reversed_order():
+    arr2 = np.array([11,23,12,22,45,65,78,98,76,56,35,49,59,27,44,1,4,7,9,3,78,6])
+    sorted_list_reversed = np.sort(arr2)[::-1]
+    print(sorted_list_reversed)
+sort_by_numpy_in_reversed_order()
